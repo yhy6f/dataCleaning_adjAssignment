d544d4d4338a081d436d338c25fa7cd322868005
Jasmine Ye Han <jasmine.han@foxmail.com>
2016-03-06 22:38:46 -0600

    deleted unnecessary comments

--- cleaner.py
+++ cleaner.py
@@ -14,12 +14,6 @@ writer = csv.DictWriter(outfile, reader.fieldnames)
 writer.writeheader()
 
 # Clean and write the data to output
-#for row in reader:
-    #row['first_name'] = row['first_name'].upper()
-    #row["zip"]=row["zip"].zfill(5)
-    #row['city'] = row['city'].replace('&nbsp;', '_')
-    #if int(row['amount']) > 1000.0: 
-        #writer.writerow(row)
 for row in reader:
     row["first_name"]=row["first_name"].upper()
     row["zip"]=row["zip"].zfill(5)
